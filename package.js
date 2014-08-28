aa727ad4f101e5e56847ad300e2c830a33b45617
Timothy Pfafman <tim@pfafman.com>
2014-08-27 22:22:02 -0700

    for 0.9

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: 'Google API loader packaged for Meteor'
+  summary: 'Google API loader packaged for Meteor',
+  version: "0.1.0",
+  git: "https://github.com/pfafman/meteor-google-loader.git"
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR@0.9.0");
 
   // Client
   api.use(['jquery', 'session'], 'client');
@@ -11,7 +14,6 @@ Package.on_use(function (api, where) {
 
 
   if (api.export) {
-    // ???  
     api.export('GoogleLoader', ['client']); 
   }
 
